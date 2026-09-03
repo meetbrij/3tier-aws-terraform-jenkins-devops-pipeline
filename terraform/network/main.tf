7742860dfda403550a916a482326d0189f8caa3c
brijesh <meetbrij@gmail.com>
2026-09-03 15:38:49 +0530

    removing nat from db

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -146,11 +146,6 @@ resource "aws_route_table" "database" {
   count  = length(var.database_subnets)
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main[count.index].id
-  }
-
   tags = {
     Name        = "${var.project_name}-database-rt-${count.index + 1}"
     Environment = var.environment
